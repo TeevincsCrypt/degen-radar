68bf4c1b522aaf347e2c4ccaa3c91d52c1b8aab1
TeevincsCrypt <teevincs1@gmail.com>
2026-03-16 11:13:41 +0100

    Update server.py

--- server.py
+++ server.py
@@ -8,10 +8,10 @@ keywords = ["AI dog", "Trump coin", "Elon pet"]
 
 def send_alert(msg):
 
-    url = f"https://api.telegram.org/bot8620436807:AAFki28rQ4gGsx8EXVgvon7AVJZRdU-mMmI/sendMessage"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
     payload = {
-        "chat_id": 7939533113,
+        "chat_id": CHAT_ID,
         "text": msg
     }
 
