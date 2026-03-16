1f653b016b31211041623943d99763336d5c3013
TeevincsCrypt <teevincs1@gmail.com>
2026-03-16 10:55:17 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,58 @@
+import requests
+import time
+
+BOT_TOKEN = "8620436807:AAFki28rQ4gGsx8EXVgvon7AVJZRdU-mMmI"
+CHAT_ID = "7939533113"
+
+keywords = ["AI dog", "Trump coin", "Elon pet"]
+
+def send_alert(msg):
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": msg
+    }
+
+    requests.post(url, json=payload)
+
+
+def scan():
+
+    for keyword in keywords:
+
+        url = f"https://api.dexscreener.com/latest/dex/search?q={keyword}"
+
+        r = requests.get(url).json()
+
+        for pair in r.get("pairs", []):
+
+            if pair["chainId"] != "solana":
+                continue
+
+            name = pair["baseToken"]["name"]
+            symbol = pair["baseToken"]["symbol"]
+            link = pair["url"]
+
+            message = f"""
+🚨 SOLANA MEME FOUND
+
+Narrative: {keyword}
+
+Token: {name} ({symbol})
+
+DEX:
+{link}
+"""
+
+            send_alert(message)
+
+            return
+
+
+while True:
+
+    scan()
+
+    time.sleep(60)
