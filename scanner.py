97ca304c8c1f582af98d72a08594ccd12614b80b
TeevincsCrypt <teevincs1@gmail.com>
2026-03-16 11:09:43 +0100

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,47 @@
+import requests
+import time
+
+BOT_TOKEN = "8620436807:AAFki28rQ4gGsx8EXVgvon7AVJZRdU-mMmI"
+CHAT_ID = "7939533113"
+
+def send(msg):
+
+    url = f"https://api.telegram.org/bot8620436807:AAFki28rQ4gGsx8EXVgvon7AVJZRdU-mMmI/sendMessage"
+
+    payload = {
+        "chat_id": 7939533113,
+        "text": msg
+    }
+
+    requests.post(url, json=payload)
+
+def scan():
+
+    url = "https://api.dexscreener.com/latest/dex/pairs/solana"
+
+    r = requests.get(url).json()
+
+    for pair in r["pairs"][:10]:
+
+        name = pair["baseToken"]["name"]
+        symbol = pair["baseToken"]["symbol"]
+        link = pair["url"]
+
+        message = f"""
+🚨 NEW SOLANA TOKEN
+
+{name} ({symbol})
+
+{link}
+"""
+
+        send(message)
+
+        break
+
+
+while True:
+
+    scan()
+
+    time.sleep(60)
